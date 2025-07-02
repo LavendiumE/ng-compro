291409d1373550ff5579de7919b68627af587e82
Nita <nitaainunnisa97@gmail.com>
2025-07-02 10:20:10 +0700

    edit filepath css dan js

--- app/Views/contact.php
+++ app/Views/contact.php
@@ -8,9 +8,9 @@
 
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href ="http://localhost:8080/css/contact.css">
-        <link rel="stylesheet" type="text/css" href ="http://localhost:8080/css/footer.css">
-        <link rel="stylesheet" type="text/css" href ="http://localhost:8080/css/navbar.css">
+        <link rel="stylesheet" type="text/css" href ="<?= base_url('css/contact.css') ?>">
+        <link rel="stylesheet" type="text/css" href ="<?= base_url('css/footer.css') ?>">
+        <link rel="stylesheet" type="text/css" href ="<?= base_url('css/navbar.css') ?>">
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Poppins:wght@300;400&display=swap" rel="stylesheet">
